55e1b0a8fb4daddfd5e28825a6241fbf5a02d3eb
Kishor Nepal <kishornepal877@gmail.com>
2024-08-11 14:01:59 +0545

    Admin module product add and admin add page validation completed.

--- BaKaSh-The_Shopping_Cart/src/main/resources/static/js/script.js
+++ BaKaSh-The_Shopping_Cart/src/main/resources/static/js/script.js
@@ -189,7 +189,161 @@ var $order=$("#orders");
 			}
 			}
 			
-			})
+			});
+			
+			
+			
+		/*	Add Admin page validation*/
+			var $addAdmin=$("#adminPageValidation");
+	$addAdmin.validate({
+		
+		rules:{
+			name:{
+				required:true,
+				lettersonly:true	
+			},
+			
+			email:{
+				required:true,
+				space:true,
+				email:true
+				
+			},
+			mobileNumber:{
+				required:true,
+				space:true,
+				numericOnly:true,
+				minlength:10,
+				maxlength:12
+			},address:{
+				required:true,
+				all:true
+			},
+			password:{
+				required:true,
+				space:true,
+				minlength:5,
+				maxlength:15,
+				strongPassword:true
+			},
+			cpassword:{
+				required:true,
+				space:true,	
+				equalTo:'#pass'
+			}
+			},
+			
+	
+			messages:{
+			name:{
+				required:' Name required',
+				lettersonly:'invalid Name'
+			},
+			
+			email:{
+				required:'email name must be requried',
+				space:'space not allowed',
+				email:'Invalid email'
+			},
+			mobileNumber:{
+				required:'mobile Number must be required',
+				space:'space not allowed',
+				numericOnly:'invalid mobile number',
+				minlength:'min 10 digit',
+				maxlength:'max 12 digit'
+			},
+			
+			address:{
+				required:'address must be required',
+				all:'invalid'
+			},
+			password:{
+				required:'passwrod must be required',
+				space:'spacce not allowed',
+				minlength:'weak password',
+				strongPassword:'add upper,lower and special letters'
+			},
+			cpassword:{
+				required:'confirm password must be required',
+				space:'space not allowed',
+				equalTo:'password mismatch'
+				
+			 }
+			
+			}
+			
+			}); 
+			
+			
+			
+			
+			/*Add Product Validation */
+			
+			var $addProduct=$("#addProductValidation");
+	$addProduct.validate({
+		
+		rules:{
+			title:{
+				required:true,
+				lettersonly:true	
+			},
+			description:{
+				required:true,
+				wordCount:[5,8]
+				
+			},
+			category:{
+				required:true,
+				
+			},
+			price:{
+				required:true,
+				space:true,
+				numericOnly:true
+			},
+			stock:{
+				required:true,
+				space:true,
+				numericOnly:true
+			},
+			file:{
+				required:true,
+			},
+		
+		},
+		messages:{
+			title:{
+				required:'title needed',
+				lettersonly:'invalid title'
+			},
+			description: {
+				required: 'Write short description',
+				wordCount:'Description must be between 5 and 8 words'
+			},
+
+			category:{
+				required:'category needed',
+			},
+			price:{
+				required:'price needed',
+				space:'spacce not allowed',
+				numericOnly:'Numbers Only'
+				
+			},
+			stock:{
+			     required:'stock needed',
+				space:'spacce not allowed',
+				numericOnly:'Numbers Only'
+				
+			},
+			file:{
+				required:'product image needed',
+			
+			}
+		}
+		
+	});
+			
 	
 	
 })
@@ -226,5 +380,11 @@ jQuery.validator.addMethod('strongPassword', function(value, element) {
     return this.optional(element) || value.length >= 8 && /[a-z]/.test(value) && /[A-Z]/.test(value) && /[0-9]/.test(value) && /[\W]/.test(value);
 }, 'Your password is not strong enough.');
 
+
+
+$.validator.addMethod("wordCount", function(value, element, params) {
+    let words = value.trim().split(/\s+/);
+    return words.length >= params[0] && words.length <= params[1];
+}, "Description must be between {0} and {1} words.");
 
 
